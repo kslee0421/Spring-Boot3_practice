5668738d3e860b9f4c8f125096acbe6f948873fa
ox161472 <ox161472@gmail.com>
2023-07-10 17:33:13 +0900

    first commit

---
+++ DepdencyInjectionSample/src/main/java/com/kslee/demo/DepdencyInjectionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.kslee.DepdencyInjectionSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepdencyInjectionSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepdencyInjectionSampleApplication.class, args);
+	}
+
+}
---
+++ DepdencyInjectionSample/src/test/java/com/kslee/DepdencyInjectionSample/DepdencyInjectionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kslee.DepdencyInjectionSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepdencyInjectionSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
